a21c770685bdeeef15ec64e0cde8e03e45c0b450
Nami-t <namitbimal@gmail.com>
2024-07-31 23:13:13 +0530

    deploy

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from index import create_app
 from index import db
-app= create_app()
+
+app=  create_app()
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
 
 with app.app_context():
     db.create_all()    
