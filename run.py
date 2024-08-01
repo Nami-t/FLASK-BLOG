c73e00c9737294e3a00e5ebceb4753cef7a8c637
Nami-t <namitbimal@gmail.com>
2024-08-01 19:41:21 +0530

    deploy

--- run.py
+++ run.py
@@ -1,16 +1,39 @@
-from index import create_app
-from index import db
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+import requests
+import logging
+from index import create_app, db
 
-app=  create_app()
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+app = create_app()
 
-if __name__=="__main__":
-    app.run(debug=True)
+backend_url = "https://flask-blog-m8jl.onrender.com"
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+def restart_server():
+    logger.info('Attempting to restart server')
+    try:
+        response = requests.get(backend_url)
+        if response.status_code == 200:
+            logger.info("Server restarted successfully")
+        else:
+            logger.error(f"Failed to restart server. Status code: {response.status_code}")
+    except requests.RequestException as e:
+        logger.error(f"Error during restart: {str(e)}")
 
+# Set up the scheduler
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=restart_server, trigger="interval", minutes=14)
+scheduler.start()
+
+# @app.route('/')
+# def home():
+#     return "Hello, World!"
 
 with app.app_context():
-    db.create_all()    
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
